49040ac8134ae5120574da1eef92fd931e0d32d0
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-02 06:02:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function doWhileLoop(integer) {
   var i = 0;
   function incrementVariable() {
     i = i + 1;
-      return i;
+    return i;
 }
   do {
     console.log("I run once regardless.");
